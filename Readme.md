632869eff38f4d3eaf8186298e0e696939260213
Chanda Narasimha Raju <workwithraju11@gmail.com>
2026-06-29 12:43:02 +0530

    Initial_commit

---
+++ Readme.md
@@ -0,0 +1,3 @@
+# Student Management System
+
+Simple Python project to manage student records.
---
+++ app.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+df=pd.read_csv(r"C:\Users\rajuc\OneDrive\Desktop\Student_Management\students.csv")
+
+print(df)
